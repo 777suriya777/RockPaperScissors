647788a356d3ccfe55e6e2c77b1bc3e7fb6c1f42
777suriya777 <suriyaprasath918@gmail.com>
2025-01-12 12:02:35 +0530

    Completed the Rock Paper Scissors game
    Created the function playRound which calculates the score per round and also created playGame which has five rounds.

--- script.js
+++ script.js
@@ -7,5 +7,37 @@ function getComputerChoice(){
 
 function getHumanChoice(){
     let userChoice = prompt("Enter either of three choices\nrock\npaper\nscissors");
-    console.log("You've entered " + userChoice);
+    return userChoice;
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === 'rock'){
+        if(computerChoice === 'paper') computerScore++;
+        else if(computerChoice === 'scissors') humanScore++;
+    }
+    else if(humanChoice === 'paper'){
+        if(computerChoice === 'rock') humanScore++;
+        else if(computerChoice === 'scissors') computerScore++;
+    }
+    else{
+        if(computerChoice === 'rock') computerScore++;
+        else if(computerChoice === 'paper') humanScore++;
+    }
+}
+
+function playGame(){
+    for(let i=0;i<5;i++){
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice,computerChoice);
+    }
+    if(humanScore > computerScore) alert("You Win");
+    else if(humanScore < computerScore) alert("You Loose");
+    else alert("Tie");
+    alert("Your Score : " + humanScore + " and Computer Score : " + computerScore);
+}
+
+playGame();
