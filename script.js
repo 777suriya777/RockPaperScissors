56027d78feb79326dc1a4dbf085256e6282ade7c
777suriya777 <suriyaprasath918@gmail.com>
2025-01-12 11:47:37 +0530

    Created function for Human Choice
    Created grtHumanChoice() function which uses prompt to get input from the user.

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3) + 1;
     return (choice === 1 ? "rock" : (choice === 2 ? "paper" : "scissors"));   
 }
+
+function getHumanChoice(){
+    let userChoice = prompt("Enter either of three choices\nrock\npaper\nscissors");
+    console.log("You've entered " + userChoice);
+}
